7572fe695a6f28f98934cf0070b816d0c45c02e0
Uma Manandhar <um_2003_8@hotmail.com>
2020-10-07 22:09:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,5 @@
 def unsafe?(speed)
-	if speed > 60
-		return true
-	elsif speed < 40
+	if speed > 60 || speed < 40
 		return true
 	else
 		return false
@@ -11,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : false 
+	speed > 60 || speed < 40 ? true : false
 end
